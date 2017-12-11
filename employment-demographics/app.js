f219a37d0bc16499baf63c2978a1c9e55e924791
Suhas Hunsimar <suhas.s.hunsimar@gmail.com>
2017-12-10 20:57:53 -0800

    Graphs Without ML
    This code base is ready to be merged with ML Algorithm.

--- employment-demographics/app.js
+++ employment-demographics/app.js
@@ -7,271 +7,190 @@ var path = require('path');
 var ejs = require('ejs');
 var app = express();
 
+// BLS Key. Necessary for communicating with V2 Version API.
+// 500 API Hits per-day. Replace with new one.
+// Registration Link for V2 - https://data.bls.gov/registrationEngine/
 var BLSkey="e22450788c9042bea997421370772602";
+var hostname = "api.bls.gov";
+var multiSeriesPath = "https://api.bls.gov/publicAPI/v2/timeseries/data";
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//app.use(express.favicon());
-//app.use(express.logger('dev'));
-//app.use(express.json());
-//app.use(express.urlencoded());
-//app.use(express.methodOverride());
-//app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.bodyParser());
-//app.use(express.cookieParser());
 
-// development only
-/*if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}*/
-
+// Initial Landing page when the server runs. Sidebar - HOME - Population - With a disability. ReferPage: welcome.ejs
 app.get('/', function(req,res){
 
-    var options = {
-        hostname: 'api.bls.gov',
-        path: 'https://api.bls.gov/publicAPI/v2/timeseries/data',
+    // Required REST API Headers for the RESTful Call.
+    var headers = {
+        hostname: hostname,
+        path: multiSeriesPath,
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         }
     };
 
-    var str='';
-
-    var postData = {
-        'seriesid':["LNU00074600", "LNU00075570", "LNU00075580", "LNU00075585", "LNU00075590", "LNU00075595", "LNU00075600"],
+    // Population - With a disability. Gender (0-1), Ethnicity (2-5), Age (6-12).
+    var populationData = {
+        'seriesid':["LNU01075630", "LNU01075704", "LNU00075557", "LNU00075558", "LNU00075560", "LNU00075559", "LNU00074600", "LNU00075570", "LNU00075580", "LNU00075585", "LNU00075590", "LNU00075595", "LNU00075600"],
         "startyear":"2016",
         "endyear":"2016",
         "registrationkey":BLSkey
     }
 
-    const req1 = https.request(options, function(response) {
-        console.log("STATUS: ${res.statusCode}");
-        console.log("HEADERS: ${JSON.stringify(res.headers)}");
-        response.setEncoding('utf8');
+    httpRESTRequest(headers, populationData, res, "welcome");
+});
 
+// Sidebar - EMPLOYMENT. ReferPage: employment.ejs
+app.get('/employment',function(req,res){
+
+    // Required REST API Headers for the RESTful Call.
+    var headers = {
+        hostname: hostname,
+        path: multiSeriesPath,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    // Education - With a disability. Pie Chart (0-3), Side-by-side Bar Chart (4-11).
+    var educationData = {
+        'seriesid':["LNU00078210", "LNU00078208", "LNU00078209", "LNU00078207", "LNU02078210", "LNU02078208", "LNU02078209", "LNU02078207", "LNU03078210", "LNU03078208", "LNU03078209", "LNU03078207"],
+        "startyear":"2016",
+        "endyear":"2016",
+        "registrationkey":BLSkey
+    }
+
+    httpRESTRequest(headers, educationData, res, "education");
+
+});
+
+// Sidebar - EDUCATION. ReferPage: education.ejs
+app.get('/education',function(req,res){
+
+    // Required REST API Headers for the RESTful Call.
+    var headers = {
+        hostname: hostname,
+        path: multiSeriesPath,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    // Education - With a disability. Pie Chart (0-3), Side-by-side Bar Chart (4-11).
+    var educationData = {
+        'seriesid':["LNU00078210", "LNU00078208", "LNU00078209", "LNU00078207", "LNU02078210", "LNU02078208", "LNU02078209", "LNU02078207", "LNU03078210", "LNU03078208", "LNU03078209", "LNU03078207"],
+        "startyear":"2016",
+        "endyear":"2016",
+        "registrationkey":BLSkey
+    }
+
+    httpRESTRequest(headers, educationData, res, "education");
+
+});
+
+// Sidebar - SALARY. ReferPage: salary.ejs
+app.get('/salary', function(req,res){
+
+    ejs.renderFile("./views/salary.ejs",function (err, result) {
+            if (!err) {
+                res.end(result);
+            }
+            else{
+                res.end('An error occurred');
+                console.log(err);
+            }
+        }
+    )
+});
+
+// Express Server port listener.
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+// Call this method to make a RESTful Web Service call to the provided - Headers & API Body Data.
+// pageName - refers to which page and graph should be rendered.
+function httpRESTRequest(headers, apiBodyData, res, pageName) {
+
+    // To store the JSON Response from the BLS Website.
+    var jsonResponse = "";
+
+    // HTTP Request object directed to the Endpoint API Headers. Also, handles the JSON Response from the server.
+    const httpRequest = https.request(headers, function (response) {
+
+        // console.log("HTTP Response Status: ${response.statusCode}");
+        // console.log("HTTP JSON Response: ${JSON.stringify(res.headers)}");
+
+        response.setEncoding('utf8');
         response.on('data', function (chunk) {
-            str += chunk;
+            jsonResponse += chunk;
         });
 
         response.on('end', function () {
-            var results = JSON.parse(str);
+            var results = JSON.parse(jsonResponse);
             console.log(results.Results.series[0].seriesID);
             var results1 = results.Results.series;
             console.log(results1);
-            ejs.renderFile("./views/welcome.ejs",{ data:results1 },function (err, result) {
-                    if (!err) {
-                        res.end(result);
-                    }
-                    else{
-                        res.end('An error occurred');
-                        console.log(err);
-                    }
-                }
-            )
+
+            renderPage(res, results1, pageName);
+
         });
     });
 
-    req1.on('error', function(e) {
+    httpRequest.write(JSON.stringify(apiBodyData)); // Writes the JSON Body data on the open connection.
+    httpRequest.on('error', function (e) {
         console.error("problem with request: ${e.message}");
     });
+    httpRequest.end(); // Closes the HTTP Request connection.
+}
 
-    req1.write(JSON.stringify(postData));
-    req1.end();
+// Call this method to render HOME page with graphs.
+function renderPage(res, graphResult, pageName) {
 
-});
-
+    var targetPage = "./views/" + pageName + ".ejs";
+    ejs.renderFile(targetPage,{ data: graphResult},function (err, result) {
+            if (!err) {
+                res.end(result);
+            }
+            else{
+                res.end('An error occurred');
+                console.log(err);
+            }
+        }
+    )
+}
 
 app.get('/login', function(req,res){
-/*
- var spawn = require('child_process').spawn,
-    py    = spawn('python', ['ml_model_evaluate.py']),
-    data = [1,3],
-    dataString = '';
+    /*
+     var spawn = require('child_process').spawn,
+        py    = spawn('python', ['ml_model_evaluate.py']),
+        data = [1,3],
+        dataString = '';
 
-py.stdout.on('data', function(data){
-  dataString += data.toString();
-});
-py.stdout.on('end', function(){
-  console.log('Sum of numbers=',dataString);
-});
-py.stdin.write(JSON.stringify(data));
-py.stdin.end();
+    py.stdout.on('data', function(data){
+      dataString += data.toString();
+    });
+    py.stdout.on('end', function(){
+      console.log('Sum of numbers=',dataString);
+    });
+    py.stdin.write(JSON.stringify(data));
+    py.stdin.end();
 
-*/
-        ejs.renderFile("./views/login.ejs",function (err, result) {
-                if (!err) {
-                    res.end(result);
-                }
-                else{
-                    res.end('An error occurred');
-                    console.log(err);
-                }
+    */
+    ejs.renderFile("./views/login.ejs",function (err, result) {
+            if (!err) {
+                res.end(result);
             }
-        )
-});
-
-app.get('/salary', function(req,res){
-
-        ejs.renderFile("./views/salary.ejs",function (err, result) {
-                if (!err) {
-                    res.end(result);
-                }
-                else{
-                    res.end('An error occurred');
-                    console.log(err);
-                }
+            else{
+                res.end('An error occurred');
+                console.log(err);
             }
-        )
-});
-
-
-//Plot gender total
-app.get('/gender',function(req,res){
-
-    var options = {
-        hostname: 'api.bls.gov',
-        path: 'https://api.bls.gov/publicAPI/v2/timeseries/data',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
         }
-    };
-
-    var str='';
-
-    var postData = {
-        'seriesid':["LNU02075630","LNU02075704"],
-        "startyear":"2015",
-        "endyear":"2016",
-        "registrationkey":BLSkey
-    }
-
-    const req1 = https.request(options, function(response) {
-        console.log("STATUS: ${res.statusCode}");
-    console.log("HEADERS: ${JSON.stringify(res.headers)}");
-    response.setEncoding('utf8');
-
-        response.on('data', function (chunk) {
-            //console.log(chunk);
-            str += chunk;
-        });
-        //var results = JSON.stringify(str);
-        //console.log(results.Results.series[0].seriesID);
-
-
-        //console.log(JSON.parse(JSON.stringify(str)));
-        response.on('end', function () {
-            //console.log(JSON.parse(JSON.stringify(str)));
-            var results = JSON.parse(str);
-            console.log(results.Results.series[0].seriesID);
-            var results1 = results.Results.series;
-            ejs.renderFile("./views/gender.ejs",{ data:results1 },function (err, result) {
-                    if (!err) {
-                        res.end(result);
-                    }
-                    else{
-                        res.end('An error occurred');
-                        console.log(err);
-                    }
-                }
-            )
-            //console.log(str);
-        });
-});
-
-    req1.on('error', function(e) {
-        console.error("problem with request: ${e.message}");
-});
-
-// write data to request body
-    var postData = {
-        "seriesid":["LNU02075630","LNU02075704"],
-        "startyear":"2016",
-        "endyear":"2016",
-        "registrationkey":"e22450788c9042bea997421370772602"
-    }
-    req1.write(JSON.stringify(postData));
-    req1.end();
-      console.log(str);
-
-});
-
-app.get('/education',function(req,res){
-
-    var options = {
-        hostname: 'api.bls.gov',
-        path: 'https://api.bls.gov/publicAPI/v2/timeseries/data',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-
-    var str='';
-
-    var postData = {
-        'seriesid':["LNU00078209", "LNU00078207", "LNU00078208", "LNU00078210"],
-        "startyear":"2015",
-        "endyear":"2016",
-        "registrationkey":BLSkey
-    }
-
-    const req1 = https.request(options, function(response) {
-        console.log("STATUS: ${res.statusCode}");
-    console.log("HEADERS: ${JSON.stringify(res.headers)}");
-    response.setEncoding('utf8');
-
-        response.on('data', function (chunk) {
-            //console.log(chunk);
-            str += chunk;
-        });
-        //var results = JSON.stringify(str);
-        //console.log(results.Results.series[0].seriesID);
-
-
-        //console.log(JSON.parse(JSON.stringify(str)));
-        response.on('end', function () {
-            //console.log(JSON.parse(JSON.stringify(str)));
-            var results = JSON.parse(str);
-            console.log(results.Results.series[0].seriesID);
-            var results1 = results.Results.series;
-            ejs.renderFile("./views/education.ejs",{ data:results1 },function (err, result) {
-                    if (!err) {
-                        res.end(result);
-                    }
-                    else{
-                        res.end('An error occurred');
-                        console.log(err);
-                    }
-                }
-            )
-            //console.log(str);
-        });
-});
-
-    req1.on('error', function(e) {
-        console.error("problem with request: ${e.message}");
-});
-
-// write data to request body
-    var postData = {
-        "seriesid":["LNU00078209", "LNU00078207", "LNU00078208", "LNU00078210"],
-        "startyear":"2016",
-        "endyear":"2016",
-        "registrationkey":"e22450788c9042bea997421370772602"
-    }
-    req1.write(JSON.stringify(postData));
-    req1.end();
-      console.log(str);
-
-});
-
-
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    )
 });
